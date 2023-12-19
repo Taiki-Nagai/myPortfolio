f2deb9c64b8df3279a79f0641cda131c34ebffd4
Taiki-Nagai <taikinagai5526@gmail.com>
2023-12-19 20:06:10 +0900

    add:footerにリンクを追加した

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import "../assets/css/Footer.css";
 import Container from "./Container";
+import Spacer from "./Spacer";
 
 const Footer = () => {
   const footerMenu: string = "Gallery"; // とりあえず今は一つだけ
@@ -10,6 +11,24 @@ const Footer = () => {
       <Container>
         <div className="footer">
           <p className="footer-menu">{footerMenu}</p>
+          <Spacer size={20} horizontal={true} />
+          <a
+            className="links__header-link"
+            href="https://github.com/Taiki-Nagai?tab=repositories"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          <Spacer size={20} horizontal={true} />
+          <a
+            className="links__header-link"
+            href="https://qiita.com/taikiti"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Qiita
+          </a>
         </div>
         <div className="footer-texts">
           <p className="footer-text">{footerText}</p>
