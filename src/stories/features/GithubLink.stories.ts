b11963f34f3f386197b67192a7d497b5f9d80b1f
Taiki-Nagai <taikinagai5526@gmail.com>
2023-12-20 09:51:03 +0900

    add:githubのリンクテキストを追加した

---
+++ src/stories/features/GithubLink.stories.ts
@@ -0,0 +1,15 @@
+import GithubLink from "@/components/features/GithubLink";
+import { Meta, StoryObj } from "@storybook/react";
+import "../../assets/css/MainSection.css";
+
+const meta = {
+  title: "GithubLink",
+  component: GithubLink,
+} satisfies Meta<typeof GithubLink>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const StoryGithubLink: Story = {
+  args: { color: "black" },
+};
