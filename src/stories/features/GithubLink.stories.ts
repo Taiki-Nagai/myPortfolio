602b23cec9b2cd920cfe2a5c95da4ca69cccd396
Taiki-Nagai <taikinagai5526@gmail.com>
2023-12-20 10:17:06 +0900

    fix:軽微な修正

--- src/stories/features/GithubLink.stories.ts
+++ src/stories/features/GithubLink.stories.ts
@@ -10,6 +10,4 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const StoryGithubLink: Story = {
-  args: { color: "black" },
-};
+export const StoryGithubLink: Story = { args: { color: "black" } };
