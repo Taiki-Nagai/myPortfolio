9bb91fe98672e4cd92c7e14749a8342ffe7af3ee
Taiki-Nagai <taikinagai5526@gmail.com>
2023-12-20 13:02:03 +0900

    fix: テストはいったん削除した

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import MainSection from "@/components/MainSection";
 import "./App.css";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import MainSection from "./components/MainSection";
 
 const App = () => {
   return (
